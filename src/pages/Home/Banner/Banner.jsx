cfe4feb7e6e27de44f52e96734a3eab50b35442b
mdYeasinIslam <hyeasinislam@gmail.com>
2026-05-19 19:40:35 +0600

    update banner image

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -1,31 +1,33 @@
-import React from "react";
-import img1 from "../../../assets/image/Banner/banner1.jpeg";
-import img2 from "../../../assets/image/Banner/banner-2.jpg";
-import img3 from "../../../assets/image/Banner/banner3.avif";
+// import img1 from "../../../assets/image/Banner/banner1.jpeg";
+// import img2 from "../../../assets/image/Banner/banner-2.jpg";
+// import img3 from "../../../assets/image/Banner/banner3.avif";
 import BannerItem from "./BannerItem";
 
 const Banner = () => {
     
     const sliderImg = [
-        {
-            image : img1,
-            prev :3,
-            id:1,
-            next :2
-        },
-        {
-            image : img2,
-            prev :1,
-            id:2,
-            next :3
-        },
-        {
-            image : img3,
-            prev :2,
-            id:3,
-            next :1
-        },
-    ]
+      {
+        image:
+          "https://i.ibb.co.com/bMYfQjqV/premium-photo-1675039871449-62f86fb78a70.avif",
+        prev: 3,
+        id: 1,
+        next: 2,
+      },
+      {
+        image:
+          "https://i.ibb.co.com/5hwm6GhY/pexels-musaddek-sayek-2152825668-33684437.jpg",
+        prev: 1,
+        id: 2,
+        next: 3,
+      },
+      {
+        image:
+          "https://i.ibb.co.com/RTKxC5DT/Sundarban-Day-Trip-1-Day-Jungle-Safari-scaled.webp",
+        prev: 2,
+        id: 3,
+        next: 1,
+      },
+    ];
   return (
     <div className="carousel w-full  
      ">
